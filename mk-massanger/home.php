2202149923504a6da7afd0ad6e68c8517242b6ba
MarouanKhellali <94488340+MarouanKhellali@users.noreply.github.com>
2023-03-08 14:56:45 +0100

    Add files via upload

---
+++ mk-massanger/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <title>MK-chat</title>
+</head>
+<body>
+  <section class="home">
+    <div class="wrapper" >
+    <header> <span>MK</span>-massanger </header>
+      <p class="intro"> welcome to our chat application</p>
+      <div class="link"> get close to your mates<a href="index.php"> signup now</a></div>
+     <div class="link"> already signed up ?<a href="login.php">login now </a></div>
+     </div>
+  </section>
+    
+</body>
+
+</html>
